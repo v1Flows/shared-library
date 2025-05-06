d041fbbdd542fb6d3ceef7511200a764b33f6a05
Justin Neubert <neubert_justin@outlook.com>
2025-05-06 10:28:00 +0200

    feat: add FailurePipelines field to Flows struct and define FailurePipeline type

--- pkg/models/flows.go
+++ pkg/models/flows.go
@@ -10,21 +10,22 @@ import (
 type Flows struct {
 	bun.BaseModel `bun:"table:flows"`
 
-	ID                  uuid.UUID `bun:",pk,type:uuid,default:gen_random_uuid()" json:"id"`
-	Name                string    `bun:"name,type:text,notnull" json:"name"`
-	Description         string    `bun:"description,type:text,default:''" json:"description"`
-	ProjectID           string    `bun:"project_id,type:text,notnull" json:"project_id"`
-	RunnerID            string    `bun:"runner_id,type:text,default:''" json:"runner_id"`
-	ExecParallel        bool      `bun:"exec_parallel,type:bool,default:false" json:"exec_parallel"`
-	Actions             []Action  `bun:"type:jsonb,default:jsonb('[]')" json:"actions"`
-	Maintenance         bool      `bun:"maintenance,type:bool,default:false" json:"maintenance"`
-	MaintenanceMessage  string    `bun:"maintenance_message,type:text,default:''" json:"maintenance_message"`
-	Disabled            bool      `bun:"disabled,type:bool,default:false" json:"disabled"`
-	DisabledReason      string    `bun:"disabled_reason,type:text,default:''" json:"disabled_reason"`
-	CreatedAt           time.Time `bun:"created_at,type:timestamptz,default:now()" json:"created_at"`
-	UpdatedAt           time.Time `bun:"updated_at,type:timestamptz" json:"updated_at"`
-	EncryptActionParams bool      `bun:"encrypt_action_params,type:bool,default:true" json:"encrypt_action_params"`
-	EncryptExecutions   bool      `bun:"encrypt_executions,type:bool,default:true" json:"encrypt_executions"`
+	ID                  uuid.UUID         `bun:",pk,type:uuid,default:gen_random_uuid()" json:"id"`
+	Name                string            `bun:"name,type:text,notnull" json:"name"`
+	Description         string            `bun:"description,type:text,default:''" json:"description"`
+	ProjectID           string            `bun:"project_id,type:text,notnull" json:"project_id"`
+	RunnerID            string            `bun:"runner_id,type:text,default:''" json:"runner_id"`
+	ExecParallel        bool              `bun:"exec_parallel,type:bool,default:false" json:"exec_parallel"`
+	Actions             []Action          `bun:"type:jsonb,default:jsonb('[]')" json:"actions"`
+	Maintenance         bool              `bun:"maintenance,type:bool,default:false" json:"maintenance"`
+	MaintenanceMessage  string            `bun:"maintenance_message,type:text,default:''" json:"maintenance_message"`
+	Disabled            bool              `bun:"disabled,type:bool,default:false" json:"disabled"`
+	DisabledReason      string            `bun:"disabled_reason,type:text,default:''" json:"disabled_reason"`
+	CreatedAt           time.Time         `bun:"created_at,type:timestamptz,default:now()" json:"created_at"`
+	UpdatedAt           time.Time         `bun:"updated_at,type:timestamptz" json:"updated_at"`
+	EncryptActionParams bool              `bun:"encrypt_action_params,type:bool,default:true" json:"encrypt_action_params"`
+	EncryptExecutions   bool              `bun:"encrypt_executions,type:bool,default:true" json:"encrypt_executions"`
+	FailurePipelines    []FailurePipeline `bun:"type:jsonb,default:jsonb('[]')" json:"failure_pipelines"`
 }
 
 type Action struct {
@@ -51,4 +52,11 @@ type Params struct {
 	Value       string   `json:"value"`
 	Default     string   `json:"default"`
 	Options     []string `json:"options"`
+}
+
+type FailurePipeline struct {
+	ID           uuid.UUID `json:"id"`
+	Name         string    `json:"name"`
+	Actions      []Action  `json:"actions"`
+	ExecParallel bool      `json:"exec_parallel"`
 }
