7e58c6fcbabb2bbf1629aaea9db744b66ab51f0e
Justin Neubert <neubert_justin@outlook.com>
2025-05-07 12:30:49 +0200

    feat: update Params struct to use Options type for Options field

--- pkg/models/flows.go
+++ pkg/models/flows.go
@@ -45,15 +45,20 @@ type Action struct {
 }
 
 type Params struct {
-	Key         string   `json:"key"`
-	Title       string   `json:"title"`
-	Description string   `json:"description"`
-	Category    string   `json:"category"`
-	Required    bool     `json:"required"`
-	Type        string   `json:"type"`
-	Value       string   `json:"value"`
-	Default     string   `json:"default"`
-	Options     []string `json:"options"`
+	Key         string    `json:"key"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Category    string    `json:"category"`
+	Required    bool      `json:"required"`
+	Type        string    `json:"type"`
+	Value       string    `json:"value"`
+	Default     string    `json:"default"`
+	Options     []Options `json:"options"`
+}
+
+type Options struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
 }
 
 type FailurePipeline struct {
